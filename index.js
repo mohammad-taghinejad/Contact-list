e7c3fb8b57ab366237aa5ee6eb15e56a70418b0f
Mohammad Taghinejad <smt.pv.page@gmail.com>
2024-03-29 20:06:04 +0330

    Perf: Get contact list

---
+++ index.js
@@ -0,0 +1,20 @@
+import readline from "readline/promises";
+import { stdin as input, stdout as output } from "process";
+
+const rl = readline.createInterface({ input, output });
+
+console.log("---- ContactList ----");
+const contactList = [];
+const firstName = await rl.question("First Name: ");
+const lastName = await rl.question("Last Name: ");
+const newContact = {
+    id: contactList.length,
+    firstName,
+    lastName,
+}
+contactList.push(newContact);
+
+console.log("Contact List: ");
+console.log(contactList);
+
+rl.close();
