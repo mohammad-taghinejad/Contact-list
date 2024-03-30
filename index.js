0c482d75be4ec60a43ccb9bd81ec313fd74eb5e7
Mohammad Taghinejad <smt.pv.page@gmail.com>
2024-03-30 20:06:35 +0330

    Perf: Improve our codeing.

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 import readline from "readline/promises";
 import { stdin as input, stdout as output } from "process";
+import { futimesSync } from "fs";
 
 const rl = readline.createInterface({ input, output });
 
-console.log("---- ContactList ----");
 const contactList = [];
-const firstName = await rl.question("First Name: ");
-const lastName = await rl.question("Last Name: ");
-const newContact = {
-    id: contactList.length,
-    firstName,
-    lastName,
+
+console.log("---- ContactList ----");
+
+
+async function addNewContact() {
+    const firstName = await rl.question("First Name: ");
+    const lastName = await rl.question("Last Name: ");
+    const newContact = {
+        id: contactList.length,
+        firstName,
+        lastName,
+    }
+    contactList.push(newContact);
 }
-contactList.push(newContact);
-const formattedContactList = contactList.map(({ id, firstName, lastName }) => `#${id} ${firstName} ${lastName}`).join(' \n ');
 
-console.log("Contact List: ");
-console.log(formattedContactList);
+function showContactList() {
+    const formattedContactList = contactList.map(({ id, firstName, lastName }) => `#${id} ${firstName} ${lastName}`).join(' \n ');
 
-rl.close();
+    console.log("Contact List: ");
+    console.log(formattedContactList);
+}
+
+function quit() {
+    rl.close();
+}
+
+await addNewContact();
+showContactList();
+quit();
